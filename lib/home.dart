f52a68989e0da10f76fc12fcc72206baa0010b20
JORGE_ARTURO_MEZA_MENDEZ <a.21308051280504@cbtis128.edu.mx>
2024-04-12 22:11:50 +0000

    Actualizado

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:meza/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const String routeName = '/home';
+
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Inicio Jorge Meza"),
+        backgroundColor: const Color(0xff8b94ea),
+      ),
+      drawer: const DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: const Color(0xff211dff),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: const Color(0xff6644ff),
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: const Color(0xff323cc5),
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
